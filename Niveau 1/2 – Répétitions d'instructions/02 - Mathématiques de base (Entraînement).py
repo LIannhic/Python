ae3f0455550dca72fbdba10d046abc9ad2a98530
LIannhic <154102382+LIannhic@users.noreply.github.com>
2024-07-15 16:49:20 +0200

    Ajout solution problème
    Réponse du problème de "Niveau 1", "Répétition d'instructions", "Mathématiques de base".

---
+++ Niveau 1/2 – Répétitions d'instructions/02 - Mathématiques de base (Entraînement).py
@@ -0,0 +1,9 @@
+# Corrigez les erreurs contenues dans le programme ci-dessous afin
+# qu'il affiche 13 fois de suite le texte 9 * 8 = 72.
+#   for loop in range(13)
+#   print("9 * 8 = 72)
+
+# Manque les deux points à la fin de la boucle
+for loop in range(13):
+   # Manque l'identation de la fonction dans la boucle et les guillemet à la fin de la parenthèse 
+   print("9 * 8 = 72")
