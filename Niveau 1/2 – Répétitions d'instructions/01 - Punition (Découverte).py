c924dfd64f75c2a55ed854d834ee5beafbd8ab32
LIannhic <154102382+LIannhic@users.noreply.github.com>
2024-07-15 16:45:03 +0200

    Ajout solution problème et dossier
    Création d'un dossier pour les réponses de la partie Répétition d'instructions.
    Réponse du problème de "Niveau 1", "Répétitions d'instructions", "Punition".

---
+++ Niveau 1/2 – Répétitions d'instructions/01 - Punition (Découverte).py
@@ -0,0 +1,10 @@
+# Votre programme doit écrire 135 fois la phrase : "Je dois respecter le Grand Sorcier.",
+# en plaçant cette phrase exactement une fois sur chaque ligne. Attention, si votre programme
+# n'affiche pas exactement cette phrase avec les points et la majuscule là où il faut,
+# il faudra tout recommencer.
+
+# créer un boucle qui se répète 135 fois
+for loop in range(135):
+   # ajouter dans la boucle la fonction pour afficher un message
+   print("Je dois respecter le Grand Sorcier.")
+    
